cd72233fbf6bfb2f8aab015849a87e6228d44199
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2019-04-24 15:25:04 +0200

    Bugfix command for weave networking

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 ​
 # disable swap
-sudo dphys-swapfile swapoff && \
-sudo dphys-swapfile uninstall && \
+sudo dphys-swapfile swapoff
+sudo dphys-swapfile uninstall
 sudo update-rc.d dphys-swapfile remove
 ​
 #install kube
 ​
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
-sudo apt-get update -q && \
-sudo apt-get install -qy kubeadm
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -q
+sudo apt-get install -qy --allow-unauthenticated kubeadm
 
 echo "reboot server with 'sudo reboot now'"
